6f2c4a7b0fc61fb937c6c6e7bdba8cb169d3b726
Zamirbek <zamirbek.alimbekov3@gmail.com>
2022-10-12 12:59:34 +0600

    Dmitriy Task1 solution

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+const executeMethod = (obj, keys, method, params) => {
+  let copyObj = { ...obj };
+  let arr = keys.split(".");
+  for (let i = 0; i < arr.length; i++) {
+    if (copyObj[arr[i]] === undefined) return null;
+    copyObj = copyObj[arr[i]];
+  }
+  return copyObj[method](...params);
+};
+module.exports = executeMethod;
